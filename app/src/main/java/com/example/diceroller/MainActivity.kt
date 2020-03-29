957699c065faf769824efdd8bd0741adaa9c84d9
ESHAGibalov <wrestakk145@gmail.com>
2020-03-29 21:30:50 +0300

    v 1.0

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,6 +13,8 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +25,52 @@ class MainActivity : AppCompatActivity() {
         }
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
+        val randomInt1 = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.urov
+            2 -> R.drawable.shef
+            3 -> R.drawable.protass
+            4 -> R.drawable.kulyash
+            5 -> R.drawable.hud
+            6 -> R.drawable.dshasg
+            7-> R.drawable.white
+            else-> R.drawable.zhenashefa
+        }
+
+        val drawableResource1 = when (randomInt1) {
+            1 -> R.drawable.urov
+            2 -> R.drawable.shef
+            3 -> R.drawable.protass
+            4 -> R.drawable.kulyash
+            5 -> R.drawable.hud
+            6 -> R.drawable.dshasg
+            7-> R.drawable.white
+            else-> R.drawable.zhenashefa
+        }
+
+
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.urov
+            2 -> R.drawable.shef
+            3 -> R.drawable.protass
+            4 -> R.drawable.kulyash
+            5 -> R.drawable.hud
+            6 -> R.drawable.dshasg
+            7-> R.drawable.white
+            else-> R.drawable.zhenashefa
         }
         diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
+
+
     }
 }
